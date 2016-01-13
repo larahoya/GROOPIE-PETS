291a541499a1840e8151130f57776aa2aaa6ecd8
Lara Hoya <larascully@hotmail.com>
2016-01-13 19:32:05 +0100

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'site#home'
+  root :to => 'site#home'
   devise_for :users, path: '',
                      path_names: {sign_in: 'login', sign_up: 'signup'} 
 end
