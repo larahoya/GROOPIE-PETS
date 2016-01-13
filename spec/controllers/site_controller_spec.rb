5e16b98e456b497483ead40f20846727189440ce
Lara Hoya <larascully@hotmail.com>
2016-01-13 17:04:05 +0100

    site controller spec

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SiteController, type: :controller do
-
+  describe "GET #home" do
+    it "render the home templace" do
+      get :home
+      expect(response).to render_template(:home)
+    end
+  end
 end
