a291a51c0e854081e3b551a94faa768618f7b42c
Lara Hoya <larascully@hotmail.com>
2016-01-14 16:58:47 +0100

    pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'site#home'
   devise_for :users, path: '',
-                     path_names: {sign_in: 'login', sign_up: 'signup'} 
+                     path_names: {sign_in: 'login', sign_up: 'signup'}
+  resources :pet, only: [:new, :create, :index, :show, :update] 
 end
