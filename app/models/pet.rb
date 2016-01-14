a84282a590af6c3773f3f2e4b045aa5bdcf2678d
Lara Hoya <larascully@hotmail.com>
2016-01-14 16:55:35 +0100

    pet validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates :name, :species,  presence: true
+  validates :age, numericality: {only_integer: true, greater_than: -1}
 end
--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
-  factory :pet do
-    name "MyString"
-species "MyString"
-age 1
-  end
 
+  factory :pet do |f|
+    f.name {Faker::Name.first_name}
+    f.species {Faker::Lorem.word}
+    f.age {Faker::Number.number(1)}
+  end
+  
 end
+
