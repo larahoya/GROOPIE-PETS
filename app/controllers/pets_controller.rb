0e62dbf20d1ebb171ebc2912de240d6041664b5c
Lara Hoya <larascully@hotmail.com>
2016-01-14 19:33:39 +0100

    pets create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,11 +2,32 @@ class PetsController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :index]
 
   def index
+    @user = current_user
     @pets = Pet.all
   end
 
   def new
-    @pet = Pet.new
+    @user = current_user
+    @pet = current_user.pets.new
+  end
+
+  def create
+    @user = current_user
+    @pet = current_user.pets.new(pet_params)
+    if @pet.save
+      flash[:notice] = 'You have a new pet!'
+      redirect_to action: 'profile', controller: 'users'
+    else
+      flash[:alert] = "Pet couldn't be created"
+      @errors = @pet.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :age, :avatar)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root :to => 'pets#home'
+  root :to => 'pets#index'
   devise_for :users, path: '',
                      path_names: {sign_in: 'login', sign_up: 'signup'}
   resources :pets, only: [:new, :create, :index, :show, :update, :destroy]
   get '/profile' => 'users#profile'
-   
+
 end
